27ec1ac3a59a3d52111812e8ff274f0ccd9f68e3
Ryan Hart <ryan@Ryans-MacBook-Pro.local>
2019-09-14 20:48:05 -0400

    created files folders

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name_) VALUES ('Beef');
+INSERT INTO burgers (name_) VALUES ('Turkey');
+INSERT INTO burgers (name_) VALUES ('Veggie');
+INSERT INTO burgers (name_, devoured) VALUES ('Avocado Provolone', true);
+INSERT INTO burgers (name_, devoured) VALUES ('Bacon Jalapeno', true);
+INSERT INTO burgers (name_, devoured) VALUES ('Lettuce Wrap', true);
